22c3b6cb20470ba053ea0b2f8c2cc1802f7f20e9
haripermadi <harinurpermadi@gmail.com>
2020-07-03 11:21:18 +0700

    update formatDate helper

--- client/src/helper/index.js
+++ client/src/helper/index.js
@@ -1,21 +1,21 @@
 export const formatDate = date => {
   let diffInSecond = (Date.now() - new Date(date)) / 1000;
   let diff;
-  //year 60*60*24*365
-  diff = Math.floor(diffInSecond / 31536000);
-  // console.log(diff);
-  if (diff >= 1) {
-    return `${diff} ${diff > 1 ? 'years' : 'year'} ago`;
-  }
-  //month 60*60*24*30
-  diff = Math.floor(diffInSecond / 2592000);
-  if (diff >= 1) {
-    return `${diff} ${diff > 1 ? 'months' : 'month'} ago`;
-  }
+  // //year 60*60*24*365
+  // diff = Math.floor(diffInSecond / 31536000);
+  // // console.log(diff);
+  // if (diff >= 1) {
+  //   return `${diff} ${diff > 1 ? 'years' : 'year'} ago`;
+  // }
+  // //month 60*60*24*30
+  // diff = Math.floor(diffInSecond / 2592000);
+  // if (diff >= 1) {
+  //   return `${diff} ${diff > 1 ? 'months' : 'month'} ago`;
+  // }
   //day 60*60*24
   diff = Math.floor(diffInSecond / 86400);
   if (diff >= 1) {
-    return `${diff} ${diff > 1 ? 'days' : 'day'} ago`;
+    return diff < 8 ? `${diff} ${diff > 1 ? 'days' : 'day'} ago` : date;
   }
   //minutes 60*60
   diff = Math.floor(diffInSecond / 3600);
