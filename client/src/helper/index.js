d569049b78f41e1efe2e9ccd99b2eb2e5ba0fe5d
haripermadi <harinurpermadi@gmail.com>
2020-07-03 11:22:10 +0700

    cleaning code

--- client/src/helper/index.js
+++ client/src/helper/index.js
@@ -1,17 +1,6 @@
 export const formatDate = date => {
   let diffInSecond = (Date.now() - new Date(date)) / 1000;
   let diff;
-  // //year 60*60*24*365
-  // diff = Math.floor(diffInSecond / 31536000);
-  // // console.log(diff);
-  // if (diff >= 1) {
-  //   return `${diff} ${diff > 1 ? 'years' : 'year'} ago`;
-  // }
-  // //month 60*60*24*30
-  // diff = Math.floor(diffInSecond / 2592000);
-  // if (diff >= 1) {
-  //   return `${diff} ${diff > 1 ? 'months' : 'month'} ago`;
-  // }
   //day 60*60*24
   diff = Math.floor(diffInSecond / 86400);
   if (diff >= 1) {
